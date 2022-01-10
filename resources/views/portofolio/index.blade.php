0fd050a5112ad0f8c45c1867e17c5d483f912b1a
inu847 <adin72978@gmail.com>
2022-01-10 10:33:30 +0700

    change url portofolio

--- resources/views/portofolio/index.blade.php
+++ resources/views/portofolio/index.blade.php
@@ -10,7 +10,7 @@
             PORTOFOLIO
         </div>
         <div class="card-body">
-            <iframe src="http://adinugroho16.000webhostapp.com/" frameborder="0"  width="100%" height="300"></iframe>
+            <iframe src="https://deviskaadinugroho.000webhostapp.com/" width="100%" height="900"></iframe>
         </div>
     </div>
 @endsection
